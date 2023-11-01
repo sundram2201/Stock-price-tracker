1f55022f167be2156402616c28138d663b9a7713
Sundram Chauhan <102293073+sundram2201@users.noreply.github.com>
2023-11-01 15:12:04 +0530

    Delete Back-end directory

--- Back-end/routes/stocks.js
+++
@@ -1,8 +0,0 @@
-const Router = require("express").Router();
-const { createStock, getStocks, getStockById } = require("../controllers/member");
-
-Router.route("/create-stock").post(createStock);
-Router.route("/stocks-list").get(getStocks);
-Router.route("/stockById").get(getStockById);
-
-module.exports = Router;
